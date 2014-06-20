846257147f1b6e0bc6e22b7e117047f2e11f670c
Drew Martin <drew.e.martin@gmail.com>
2014-06-20 19:12:56 -0400

    Refactoring views to use helpers and adding navigation

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,7 +6,7 @@ class PicturesController < ApplicationController
 
   	def show
   		
-	    @picture = Pictures.find(params[:id])
+	    @picture = Picture.find(params[:id])
 	  
   	end
 
@@ -15,5 +15,21 @@ class PicturesController < ApplicationController
 	end
 
 	def create
+		@picture = Picture.new(picture_params)
+		if @picture
+			@picture.save
+			redirect_to pictures_url
+		else
+			render :new 
+		end	
+	end
+
+	private
+	def picture_params
+		params.require(:picture).permit(:artist, :title, :url)
+		#this private method is used to pass a "true" 
+		#statement on the create method. "require" demands
+		#an instance of the Picture class and permits three
+		#arguments to be passed 
 	end
 end
