273bed8b8b9d57b360593dca2cf377377c6d6a6d
Drew Martin <drew.e.martin@gmail.com>
2014-06-20 16:15:13 -0400

    added dynamic paths for pictures using params

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,5 +19,32 @@ class PicturesController < ApplicationController
 	      }
 	    ]
   end
+
+  	def show
+  		@pictures = [
+	      {
+	        :title  => "The old church on the coast of White sea",
+	        :artist => "Sergey Ershov",
+	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+	      },
+	      {
+	        :title  => "Sea Power",
+	        :artist => "Stephen Scullion",
+	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+	      },
+	      {
+	        :title  => "Into the Poppies",
+	        :artist => "John Wilhelm",
+	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+	      }
+	    ]
+
+	    @picture = @pictures[params[:id].to_i]
+	    #since @pictures is an array of hashes by calling
+	    #params ID which is called throug the url bar info 
+	    #is available through indexing the array once the 
+	    #id is converted to an integer
+  	end
+
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Photogur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
+
+  get 'pictures/:id' =>'pictures#show', as: "picture" #"picture refers to prefix alias in rake routes"
 end
