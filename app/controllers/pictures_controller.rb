76d8d13e15b0a0895e274633705b8a719e156569
Drew Martin <drew.e.martin@gmail.com>
2014-06-20 18:11:18 -0400

    integrated forms within current db structure

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,52 +1,17 @@
 class PicturesController < ApplicationController
 
 	def index
-		@pictures = [
-	      {
-	        :title  => "The old church on the coast of White sea",
-	        :artist => "Sergey Ershov",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	      },
-	      {
-	        :title  => "Sea Power",
-	        :artist => "Stephen Scullion",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	      },
-	      {
-	        :title  => "Into the Poppies",
-	        :artist => "John Wilhelm",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	      }
-	    ]
-  end
+		@pictures = Picture.all
+  	end
 
   	def show
-  		@pictures = [
-	      {
-	        :title  => "The old church on the coast of White sea",
-	        :artist => "Sergey Ershov",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	      },
-	      {
-	        :title  => "Sea Power",
-	        :artist => "Stephen Scullion",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	      },
-	      {
-	        :title  => "Into the Poppies",
-	        :artist => "John Wilhelm",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	      }
-	    ]
-
-	    @picture = @pictures[params[:id].to_i]
-	    #since @pictures is an array of hashes by calling
-	    #params ID which is called throug the url bar info 
-	    #is available through indexing the array once the 
-	    #id is converted to an integer
+  		
+	    @picture = Pictures.find(params[:id])
+	  
   	end
 
 	def new
+		@picture = Picture.new
 	end
 
 	def create
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Photogur::Application.routes.draw do
 
   get 'pictures/new' => 'pictures#new'
   post 'pictures' => 'pictures#create'
-
-  get 'pictures/:id/edit' => 'pictures#edit'
+  
   post 'pictures/:id' => 'pictures#update'
+  get 'pictures/:id/edit' => 'pictures#edit'
+  
 
   get 'pictures/:id' => 'pictures#show', as: "picture" #"picture refers to prefix alias in rake routes"
 
